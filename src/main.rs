e093edc07d56177fac757d9917603478a1dd78f6
snystrom <snystrom@users.noreply.github.com>
2020-10-03 19:12:29 -0400

    Add main.rs with spec code

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+extern crate bio;
+
+use std::convert::TryInto;
+use rust_htslib::{bam, bam::Read};
+//use rust_htslib::bam;
+use structopt::StructOpt;
+use bio::io::bed;
+
+#[derive(StructOpt, Debug)]
+#[structopt(setting = structopt::clap::AppSettings::ArgRequiredElseHelp)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    bam: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    regions: std::path::PathBuf,
+
+}
+
+struct VEntry {
+    /// Start position of fragment
+    start: i64,
+    /// Insert size of the fragment
+    insert_size: i64,
+    /// End coordinate of the bed region
+    region_end: i64,
+    /// index of which bed region this fragment resides in
+    region_n: i64,
+}
+
+impl VEntry {
+    /// Create empty
+    fn allocate() -> VEntry {
+
+        VEntry {
+            start: -1,
+            insert_size: -1,
+            region_end: -1,
+            region_n: -1,
+        }
+    }
+
+    /// Midpoint of the fragment
+    fn midpoint(&self) -> i64 {
+        // NOTE: midpoint is rounded down if insert_size is odd
+        // TODO: Fix?? Round up?
+        self.start + (self.insert_size/2)
+    }
+
+    /// Row in the VMatrix this entry should be inserted into
+    fn row(&self) -> i64 {
+        // NOTE: region count has to be > 0, else will get all 0's for first region row positions
+        (self.region_n + 1) * self.insert_size.abs()
+    }
+
+    /// Column in the VMatrix this entry should be inserted into
+    fn col(&self) -> i64 {
+        self.region_end - self.midpoint()
+    }
+
+    /// Update fragment entry
+    /// Allows operation in same allocation
+    fn update(&mut self, region: &bed::Record, region_n: i64, record: &bam::Record) -> std::io::Result<()> {
+        self.start = record.pos();
+        self.insert_size = record.insert_size();
+        self.region_end = region.end().try_into().unwrap();
+        self.region_n = region_n;
+        Ok(())
+    }
+}
+
+fn main() {
+    let args = Cli::from_args();
+    let bed_reader = bed::Reader::from_file(&args.regions);
+    let mut bam_reader = bam::IndexedReader::from_path(&args.bam)
+        .ok()
+        .expect("Cannot read bam file");
+
+    let mut ventry = VEntry::allocate();
+
+    for bed_region in bed_reader.unwrap().records() {
+        let region = bed_region.ok()
+                               .expect("Error reading bed region");
+
+        //println!("{}\t{}\t{}", region.chrom(), region.start(), region.end());
+
+        let region_chrom = region.chrom();
+        let tid = bam_reader.header().tid(region_chrom.as_bytes()).unwrap();
+
+        bam_reader.fetch(tid, region.start(), region.end()).unwrap();
+
+        // read fetched records into bam_record in loop,
+        // write out to matrix
+        let mut record = bam::Record::new();
+
+        // initialize loop
+        let mut check = bam_reader.read(&mut record);
+
+        //TODO:
+        // Since I want an indexed bam file, it can't be sorted.
+        // so, I think what I need to do is for each record
+        // only save if record.is_paired(), on the same chromosome, and totally within the bed region, and insert size < size cutoff.
+        //  - create range of bed entry start/end
+        //  -
+        // Then save each midpoint of the fragment as: record.pos() + record.insert()
+        while check.unwrap() {
+
+
+            // TODO: if record is totally within region && insert < cutoff
+            // NOTE: Actually, I think only midpoint needs to be within the region, not the whole read.
+            if record.is_proper_pair() && record.is_first_in_template() {
+                // TODO: write to matrix
+                //
+                let insert = &record.insert_size();
+                let start = &record.pos();
+                let end = start + insert;
+                // TODO: currently, insert/2 rounds down
+                // Needs fix? Round up?
+                let midpoint = start + (insert / 2);
+                // TODO: to write midpoint as x matrix coord use bed_region.end() - midpoint
+
+                println!("{}\t{}\t{}\t{}\t{}", region_chrom, start, end, midpoint, insert.abs());
+            }
+
+            check = bam_reader.read(&mut record);
+        }
+
+    }
+
+}
