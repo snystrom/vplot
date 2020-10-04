e6fdb7ea7bba22cf553b1a95190b97edd38741e2
snystrom <snystrom@users.noreply.github.com>
2020-10-04 00:01:54 -0400

    Added matrix writing infrastructure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate bio;
+extern crate matrix;
 
-use std::convert::TryInto;
+use std::convert::{TryInto, TryFrom};
 use rust_htslib::{bam, bam::Read};
 //use rust_htslib::bam;
 use structopt::StructOpt;
 use bio::io::bed;
+use matrix::prelude::*;
 
 #[derive(StructOpt, Debug)]
 #[structopt(setting = structopt::clap::AppSettings::ArgRequiredElseHelp)]
@@ -52,14 +54,15 @@ impl VEntry {
     }
 
     /// Row in the VMatrix this entry should be inserted into
-    fn row(&self) -> i64 {
+    fn row(&self) -> usize {
         // NOTE: region count has to be > 0, else will get all 0's for first region row positions
-        (self.region_n + 1) * self.insert_size.abs()
+        ((self.region_n + 1) * self.insert_size.abs()).try_into().unwrap()
     }
 
     /// Column in the VMatrix this entry should be inserted into
-    fn col(&self) -> i64 {
-        self.region_end - self.midpoint()
+    // value must be usize because matrix library needs usize
+    fn col(&self) -> usize {
+        (self.region_end - self.midpoint()).try_into().unwrap()
     }
 
     /// Update fragment entry
@@ -74,14 +77,75 @@ impl VEntry {
 
 // TODO: VMatrix struct that hold matrix
 
+fn count_bed_entries(path: &std::path::PathBuf) -> i64 {
+    let bed_reader = bed::Reader::from_file(path);
+    bed_reader.unwrap()
+              .records()
+              .count() as i64
+}
+
+/// Return (n_bed_entries, bed_width)
+/// n_bed_entries * max_fragment_size = nrow
+fn get_dims_from_bed(path: &std::path::PathBuf) -> (usize, usize) {
+    // loop over records and ensure same width
+    // count records
+
+    let bed_reader = bed::Reader::from_file(path);
+    let mut n_regions = 0;
+    let mut set_width = 0;
+    let mut init = true;
+    for region in bed_reader.unwrap().records() {
+        let record = region.unwrap();
+        n_regions += 1;
+        // Grab width of first region
+        // all other regions should be this width
+        if init {
+            set_width = record.end() - record.start();
+            init = false;
+        }
+
+        let width = record.end() - record.start();
+
+        if set_width != width {
+            panic!("All bed regions must be equal width\nWidth: {} of bed entry on line {} does not match width of first entry: {}", width, n_regions, set_width);
+        }
+
+    }
+
+    (n_regions as usize, set_width as usize)
+}
+
+/// (nrow, ncol)
+fn get_matrix_dims(args: &Cli) -> (usize, usize){
+    let (n_bed, ncol) = get_dims_from_bed(&args.regions);
+    let mfs = usize::try_from(args.max_fragment_size).unwrap();
+    let nrow = n_bed * mfs;
+    (nrow, ncol)
+}
+
+fn connect_indexed_bam(path: &std::path::PathBuf) -> bam::IndexedReader {
+    bam::IndexedReader::from_path(path)
+        .ok()
+        .expect("Cannot read bam file")
+}
+
+fn initialize_matrix(nrow: usize, ncol: usize) -> Compressed<usize> {
+    Compressed::zero((nrow, ncol))
+}
+
 fn main() {
     let args = Cli::from_args();
     let bed_reader = bed::Reader::from_file(&args.regions);
-    let mut bam_reader = bam::IndexedReader::from_path(&args.bam)
-        .ok()
-        .expect("Cannot read bam file");
+    let mut bam_reader = connect_indexed_bam(&args.bam);
 
     let mut ventry = VEntry::allocate();
+
+    // TODO: remove below comments (debug)
+    //let (n_bed, ncol) = get_dims_from_bed(&args.regions);
+    //let nrow = n_bed * (args.max_fragment_size as usize);
+    let (nrow, ncol) = get_matrix_dims(&args);
+
+    let mut vmatrix = initialize_matrix(nrow, ncol);
 
     let mut bed_region_n = 0 as i64;
     for bed_region in bed_reader.unwrap().records() {
@@ -116,9 +180,12 @@ fn main() {
             // NOTE: Actually, I think only midpoint needs to be within the region, not the whole read.
             if record.is_proper_pair() && record.is_first_in_template() {
                 ventry.update(&region, bed_region_n, &record);
-                if bed_range.contains(ventry.midpoint()) && ventry.insert_size.abs() <= args.max_fragment_size {
+                if bed_range.contains(&ventry.midpoint().try_into().unwrap()) && ventry.insert_size.abs() <= args.max_fragment_size {
                     // TODO: write to matrix
-                    println!("{}\t{}\t{}", ventry.row(), ventry.col(), ventry.midpoint());
+                    let mut val = vmatrix.get((ventry.row(), ventry.col()));
+                    val += 1;
+                    vmatrix.set((ventry.row(), ventry.col()), val);
+                    //println!("{}\t{}\t{}", ventry.row(), ventry.col(), ventry.midpoint());
                 }
             }
 
@@ -129,4 +196,7 @@ fn main() {
         // Advance to next bed region
         bed_region_n += 1;
     }
+
+    // TODO: remove, print final matrix
+    println!("{:?}", vmatrix);
 }
