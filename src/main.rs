658d104bd5da3bb80b1a4e337fbc2792f0ad0b88
snystrom <snystrom@users.noreply.github.com>
2022-05-08 22:32:19 -0400

    Bump rustbio version. Closes #8.
    bed3 parsing was broken in older versions of rustbio. Now resolved with
    latest version.

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,10 @@ struct Cli {
     /// Path to an indexed bam file
     #[structopt(parse(from_os_str))]
     bam: std::path::PathBuf,
-    /// Path to a bed file (must be in bed4 format: chr, start, end, name) Of
-    /// a region (or regions) in which to generate the vplot. If using multiple
-    /// regions, all entries must be the same width. If setting multiple
-    /// regions, reads will be aggregated into a single matrix unless `--multi`
-    /// is set.
+    /// Path to a bed file of a region (or regions) in which to generate the
+    /// vplot. If using multiple regions, all entries must be the same width. If
+    /// setting multiple regions, reads will be aggregated into a single matrix
+    /// unless `--multi` is set.
     #[structopt(parse(from_os_str))]
     regions: std::path::PathBuf,
     /// Maximum fragment size to include in the V-plot matrix
